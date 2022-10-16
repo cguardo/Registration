5fd16b1bbfc3049b39cfc371d279c31f7c4c79f1
Vanessa <leoperio,vanessa@ue.edu.ph>
2022-10-17 00:31:23 +0800

    js file

--- js/main.js
+++ js/main.js
@@ -1,3 +1,38 @@
+function formValidation() {
+    var lastname = document.registration.lastname;
+    var firstname = document.registration.passid;
+    //var uname = document.registration.username;
+    //var uadd = document.registration.address;
+    //var ucountry = document.registration.country;
+    //var uzip = document.registration.zip;
+    //var uemail = document.registration.email;
+    //var umsex = document.registration.msex;
+    //ar ufsex = document.registration.fsex; if (userid_validation(uid, 5, 12)) 
+    
+        if (passid_validation(lastname, 2, 15)) {
+            if (allLetter(uname)) {
+                
+            }
+        }
+    
+    return false;
+
+} 
+
+function lastname_validation(lastname, mx, my) {
+    var lastname_len = lastname.value.length;
+    if (lastname_len == 0 || lastname_len >= my || lastname_len < mx) {
+        alert("User Id should not be empty / length be between " + mx + " to " + my);
+        lastname.focus();
+        return false;
+    }
+    return true;
+}
+
+
+
+
+
 $(function(){
     $("#form-total").steps({
         headerTag: "h2",
